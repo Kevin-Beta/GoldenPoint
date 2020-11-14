1ea21546f2979592f63fcee3557014441ffce53f
Kevin-Beta <p.body@gmail.com>
2020-11-14 20:36:22 +0800

    完成gp.cpp, 修改数据文件并另存为data.txt方便读取

--- gp.cpp
+++ gp.cpp
@@ -1,70 +1,120 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+
 struct {
 	float data;
-	float grade;
+	int grade;
 } Player[100];
 
 void game();
 
 int main() {
-	int n;
-	scanf("%d", &n);
-	if (n == 1) {
-		game();
-
-	}
+	freopen("data.txt","r",stdin);
+	game();
 	return 0;
 }
 
 void game() {
-	int  N, a, b, c, i, j, k;
+	//N人数，a游戏次数
+	int  N, a, b, c;
 
-	scanf("%d", &N);
+	float co=0.618; //黄金点系数
+	int dgrade=1;	//每次得失的分数
+	scanf("%d%d", &N, &a);
 
-	scanf("%d", &a);
-	for (i= 0; i < N; i++) {
+
+	for (int i= 0; i < N; i++) {
 		Player[i].grade = 0;
 	}
-	for (j = 1; j <= a; j++) {
+	
+	bool flag=false;
+	
+	//游戏开始
+	for (int j = 1; j <= a; j++) {
+		
+		if(flag){
+			printf("\n");
+		}else{
+			flag=true;
+		}
+		
 		float sum=0;
 		float G=0,m=0;
-
-		for (i = 0; i < N; i++) {
+		float max=-1,min=101;
+		
+		//输入每个人的数值，并计算总值
+		for (int i = 0; i < N; i++) {
 			scanf("%f", &Player[i].data);
 			if(Player[i].data>=0&&Player[i].data<=100) {
 				sum += Player[i].data;
 			}
 		}
-		G = (float)sum*0.618 / N;
+		
+		//计算黄金点
+		G = (float)sum*co / N;
 
-		for (k = 0; k < N; k++) {
-			m = abs((float)Player[k].data - G);
-			if (m>=min) {
+		//找出距离黄金点最近和最远的距离
+		for (int i = 0; i < N; i++) {
+			m = abs((float)Player[i].data - G);
+			if (m < min) {
 				min = m;
 			}
-			if (m <= max) {
+			if (m > max) {
 				max = m;
 			}
 		}
-		for(i=0; i<N; i++) {
+		
+		//找出离黄金点最近和最远的输入值，并计算分数得失
+		for(int i=0; i<N; i++) {
 			float delta=abs((float)Player[i].data - G);
-			if(delta==max) {
-				Player[i].grade-=1;
+			if(delta == max) {
+				Player[i].grade-=dgrade;
 			}
-			if(delta==min) {
-				Player[i].grade+=1;
+			if(delta == min) {
+				Player[i].grade+=dgrade;
 			}
 		}
+		
+		//输出该轮次后的分数情况
 		printf("轮次:%d\n",j);
-		for (i = 0; i < N; i++) {
-			printf("玩家%d的得分为:", i+1);
-			printf("%f\n", Player[i].grade);
+		printf("黄金点数值为:%f\n",G);
+		printf("玩家得分情况:\n");
+		for (int i = 0; i < N; i++) {
+			printf("玩家%d:%d\n", i+1,Player[i].grade);
 		}
 	}
-
+	
+	//找出得分最高和最低
+	int loser[100],winner[100],len_loser=0,len_winner=0;
+	int min_grade=101,max_grade=-1;
+	
+	for (int i = 0; i < N; i++) {
+		if (Player[i].grade < min_grade) {
+			min_grade = Player[i].grade;
+		}
+		if (Player[i].grade > max_grade) {
+			max_grade = Player[i].grade;
+		}
+	}
+	//printf("%d\n%d\n",min_grade,max_grade);
+	
+	for(int i = 0 ; i < N; i++) {
+		if(Player[i].grade == max_grade) {
+			winner[len_winner++]=i+1;  //PS:玩家编号为数组下标+1
+		}
+		if(Player[i].grade == min_grade) {
+			loser[len_loser++]=i+1;
+		}
+	}
+	
+	printf("赢家编号是:\n");
+	printf("正向得分:");
+	for(int i=0;i<len_winner;i++){
+		printf("%d%s",winner[i],(i==len_winner-1)?"\n":",");
+	}
+	printf("反向得分:");
+	for(int i=0;i<len_loser;i++){
+		printf("%d%s",loser[i],(i==len_loser-1)?"\n":",");
+	}
+	fclose(stdin);
 }
